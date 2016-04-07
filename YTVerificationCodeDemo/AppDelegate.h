8df711a8e56187c19d4028259aec3732fa9be573
TonyAng <13323650885@163.com>
2016-04-07 11:56:43 +0800

    YTVerificationCodeDemo
    不区分大小写的识图验证码，封装，易调用

---
+++ YTVerificationCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTVerificationCodeDemo
+//
+//  Created by TonyAng on 16/4/7.
+//  Copyright © 2016年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
